b8b9fbef3fdee3499809ae9dd535e62c40119b93
haristariq <tharis715@gmail.com>
2023-07-30 04:03:39 -0700

    setup

---
+++ screens/HeartScreen.js
@@ -0,0 +1,11 @@
+// HomeScreen.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function HeartScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Heart Screen</Text>
+    </View>
+  );
+}
