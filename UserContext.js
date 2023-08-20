7d869bd3df04f00fadc6858b841b12b58abce5e8
haristariq <tharis715@gmail.com>
2023-08-19 20:52:38 -0700

    checks

--- UserContext.js
+++ UserContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { getUser } from './backend';  // adjust path to your backend file
 import { getIdToken } from './firebase';  // adjust path to your firebase file
+import { auth } from './firebase';
 
 const UserDataContext = createContext();
 
@@ -22,20 +23,22 @@ export const UserDataProvider = ({ children }) => {
   // Fetch user data when the component is mounted
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const token = await getIdToken();
-        
-        const fetchedData = await getUser(token);
-        console.log("Fetched User Data:", fetchedData); // Debug: Check fetched user data
-
-        setUserData(fetchedData);
-      } catch (error) {
-        console.error('Error fetching user data:', error);
+      // Check if user is authenticated before proceeding
+      if (auth.currentUser) {
+        try {
+          const token = await getIdToken();
+          const fetchedData = await getUser(token);
+          console.log("Fetched User Data:", fetchedData);
+          setUserData(fetchedData);
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
       }
     };
 
     fetchData();
-  }, []);
+}, []);
+
 
   return (
     <UserDataContext.Provider value={{ userData, setUserData, restaurants, setRestaurants }}>
